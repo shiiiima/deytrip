21a7f42d679cc532d1f820dae1e23002c0017d04
reytonDesign <iranitshop.net@gmail.com>
2020-11-04 14:42:25 +0330

    'deytrip'

---
+++ dist/js/main.js
@@ -0,0 +1,79 @@
+function readMore1(){
+    var dots1 = document.getElementById("dots1");
+    var moreText1 = document.getElementById("more1");
+    var btnText1 = document.getElementById("moreBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText1.innerHTML = " more";
+      moreText1.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText1.innerHTML = "less";
+      moreText1.style.display = "inline";
+    }
+}
+
+function readMore2(){
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("moreBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = " more";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "less";
+    moreText2.style.display = "inline";
+  }
+}
+
+function readMore3(){
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3= document.getElementById("moreBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = " more";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "less";
+    moreText3.style.display = "inline";
+  }
+}
+
+function readMore4(){
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText4= document.getElementById("moreBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = " more";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "less";
+    moreText4.style.display = "inline";
+  }
+}
+
+function readMore5(){
+  var dots5 = document.getElementById("dot5");
+  var moreText5 = document.getElementById("more5");
+  var btnText5= document.getElementById("moreBtn5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    btnText5.innerHTML = " more";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none";
+    btnText5.innerHTML = "less";
+    moreText5.style.display = "inline";
+  }
+}
